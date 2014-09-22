0b993aabb4a0980b662307ae99bc995475bd36c1
MariaRivera <maria.rivera30@upr.edu>
2014-09-21 20:40:58 -0400

    test3

--- public/client.js
+++ public/client.js
@@ -1,4 +1,4 @@
-// app.jskkkkk
+// app.jskkkkk version2
 // create our angular app and inject ngAnimate and ui-router 
 // =============================================================================
 angular.module('formApp', ['ngAnimate', 'ui.router'])
