3aa876cb7cffab1a5498d8cfb93609cf573ae024
mariarivera30 <maria.rivera30@upr.edu>
2014-10-02 01:53:02 -0400

    phase3

---
+++ Desktop/routes/index.js
@@ -0,0 +1,13 @@
+
+//var pg = require('pg');
+//var conString = "pg://postgres:casa7463@localhost:5432/oficinasMedicas";
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' });
+};
+
+
+
