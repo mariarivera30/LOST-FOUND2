beec6cc6475bfccfcd8d75e8b8a4ccfb8fd9106c
AngelDeLaTorre <angel.delatorre@upr.edu>
2014-10-02 01:17:51 -0400

    PHASE3 DESKTOP VERSION

--- Desktop/app.js
+++ Desktop/app.js
@@ -13,7 +13,7 @@ var app = express();
 
 //var pg = require("pg");
 
-//var conString = "pg://postgres:casa7463@localhost:5432/oficinasMedicas";
+//var conString = "pg://postgres:PASS@localhost:5432/NAME";
 
 //var client = new pg.Client(conString);
 //client.connect();
@@ -54,66 +54,6 @@ http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
-
-
- //login
-app.post('/signinDoc', routes.signInDoc);
-app.get('/logoutDoc', routes.logoutDoc);
-app.post('/signinPat', routes.signInPat);
-app.post('/doctor/signup1',routes.createDoctorAccount);
-app.put('/doctor/edit/:idD',routes.editDoctor);
-app.post('/appointment/signup/new/:idP/:idD',routes.createAppointment);
-app.put('/appointment/edit/:idP/:idD',routes.editAppointment);
-app.post('/patient/signup/new',routes.createPatientAccount);
-app.put('/patient/edit/:idP',routes.editPatient);
-app.get('/logoutPat', routes.logoutPat);
-
-
-//getters 
-app.get('/doctor/all', routes.getDoctors);//ok
-app.get('/doctor/single/:id',routes.getDoctorById);//ok
-app.get('/doctor/fromPatient/:idP',routes.getDoctorsFromPatient);//ok
-app.get('/patient/single/:idP',routes.getPatientById);//ok
-app.get('/patient/fromDoctor/:idD',routes.getPatientFromDoctor);//ok
-app.get('/appointment/fromDoctor/:idD', routes.getAppointmentsFromDoc);//ok
-app.get('/appointment/fromPatient/:idP',routes.getAppointmentsFromPatient);//ok
-app.get('/appointment/single/:idA',routes.getAppointmentById);//ok
-app.get('/appointment/byDateAndDoctor/:idD/:date',routes.getAppointmentsByIdAndDate);//OK
-app.get('/appointment/pending/:idD',routes.getAppointmentsPendingFromDoctor);//ok
-app.get('/appointment/canceled/:idD/:date',routes.getAppointmentsCanceledFromDoctor);//ok
-
-app.get('/hola', routes.hola);
-
-/*function hola(){
-	
-var options = {
-  host: 'localhost',
-  port: '3000',
-  method: 'GET',
-  path: '/hola'
-};
-console.log("entro "+options);
-var req = http.get(options, function(res) {
-  console.log('STATUS: ' + res.statusCode);
-  console.log('HEADERS: ' + JSON.stringify(res.headers));
-
-  // Buffer the body entirely for processing as a whole.
-  var bodyChunks = [];
-  res.on('data', function(chunk) {
-    // You can process streamed parts here...
-    bodyChunks.push(chunk);
-  }).on('end', function() {
-    var body = Buffer.concat(bodyChunks);
-    console.log('BODY: ' + body);
-    // ...and/or process the entire body here.
-  })
-});
-
-req.on('error', function(e) {
-  console.log('ERROR: ' + e.message);
-});
-}*/
-//hola();
 
 
 
--- Desktop/routes/patient.js
+++
@@ -1,9 +0,0 @@
-exports.Patient=function(patientid, name, lastName,phone){
-	var pat ={};
-    pat.id = patientid;
-	pat.name = name;
-	pat.lastName = lastName;
-	pat.phone = phone;
-	//pat.toJSON = toJSON;
-    return pat;
-};
